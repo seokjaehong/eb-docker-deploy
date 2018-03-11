b6595334371217a0029fe24aee016e726ccb077f
seokjae hong <hsj2334@gmail.com>
2018-03-11 14:19:39 +0900

    dockerfile.local 및 관련 config 수정

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -4,6 +4,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 WSGI_APPLICATION = 'config.wsgi.local.application'
 
+INSTALLED_APPS +=[
+    'django_extensions',
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
