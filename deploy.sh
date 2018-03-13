cedbfa09ac48e473f0f2002c08329eb8ecb34be5
seokjae hong <hsj2334@gmail.com>
2018-03-13 14:57:18 +0900

    ebextension폴더 추가, migrate container_commands등록

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 #! usr/bin/env bash
 git add -f .secrets && eb deploy --staged --profile=eb && git reset HEAD .secrets
 
-ENV $containerid  sudo docker ps -q
-
-sudo docker exec -it $containerid  python srv/project/app/manage.py migrate
