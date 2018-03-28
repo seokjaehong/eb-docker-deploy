cedf76f86440921d8dbc6c16c60fa599378f8795
seokjae hong <hsj2334@gmail.com>
2018-03-29 00:25:01 +0900

    createsu수정

--- app/members/management/commands/createsu.py
+++ app/members/management/commands/createsu.py
@@ -5,11 +5,19 @@ from django.core.management import BaseCommand
 User = get_user_model()
 
 
+# class Command(BaseCommand):
+#     def handle(self, *args, **options):
+#         if not User.objects.filter(username=settings.SUPERUSER_USERNAME).exists():
+#             User.objects.create_superuser(
+#                 username=settings.SUPERUSER_USERNAME,
+#                 password=settings.SUPERUSER_PASSWORD,
+#                 email=settings.SUPERUSER_EMAIL
+#             )
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        if not User.objects.filter(username=settings.SUPERUSER_USERNAME).exists():
+    def handle(self, *args, **kwargs):
+         if not User.objects.filter(username=settings.SECRETS['SUPERUSER_USERNAME']).exists():
             User.objects.create_superuser(
-                username=settings.SUPERUSER_USERNAME,
-                password=settings.SUPERUSER_PASSWORD,
-                email=settings.SUPERUSER_EMAIL
+                username=settings.SECRETS['SUPERUSER_USERNAME'],
+                password=settings.SECRETS['SUPERUSER_PASSWORD'],
+                email=settings.SECRETS['SUPERUSER_EMAIL'],
             )
